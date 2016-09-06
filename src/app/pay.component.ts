252a9839aecb479202839940d6a1be8f40f26468
yong <zengjunyong@gmail.com>
2016-09-06 18:29:22 +0800

    what a fuck, got stuck...can't solve this bug in ng2

--- src/app/pay.component.ts
+++ src/app/pay.component.ts
@@ -1,25 +1,48 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {StripeService}     from './stripe.service';
+import {Plan} from './plan'
 
 @Component({
-  moduleId: module.id,
-  templateUrl: 'pay.component.html'
+    moduleId: module.id,
+    templateUrl: 'pay.component.html'
 })
 export class PayComponent implements OnInit, OnDestroy {
-  sub: any;
-  result: any;
+    sub: any;
+    plan: any;
+    success: boolean;
 
 
-  constructor(private route: ActivatedRoute) {
-  }
+    constructor(private route: ActivatedRoute, private stripeService: StripeService) {
+    }
 
-  ngOnInit() {
-    this.sub = this.route.params.subscribe(result => {
-      this.result = result;
-    });
-  }
+    pay() {
+        var _this = this;
+        let key = 'pk_test_FX2nzQcClgXqETUTMZDK2BNu';
+        // let key = 'pk_live_Zo2921HDEtTxDEZfXS3ZVR5N';
+        StripeCheckout.configure({
+            key: key,
+            name: 'GaiGai Pte Ltd',
+            description: this.plan.itemName,
+            image: 'https://www.letsgaigai.com/gokaikai/assets/images/big.logo.png',
+            allowRememberMe: false,
+            panelLabel: this.plan.count ? 'Subscribe' : 'Pay S$' + this.plan.amount,
+            token: function (token: any) {
+                _this.plan.tokenId = token.id;
+                _this.plan.email = token.email;
+                console.log(_this.plan);
+                _this.stripeService.pay(_this.plan).subscribe(result => _this.success = result.success);
+            }
+        }).open();
+    }
 
-  ngOnDestroy() {
-    this.sub.unsubscribe();
-  }
+    ngOnInit() {
+        this.sub = this.route.params.subscribe((plan: Plan)=> {
+            this.plan = plan;
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
 }
--- src/app/price.table.ts
+++ src/app/price.table.ts
@@ -1,15 +1,7 @@
 import {Component} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {StripeService}     from './stripe.service';
-
-interface Plan {
-    id?: string;
-    tokenId?: string;
-    email?: string;
-    amount: number;
-    itemName: string;
-    count?: number;
-}
+import {Plan} from './plan'
 
 @Component({
     moduleId: module.id,
@@ -26,22 +18,6 @@ export class PriceTable {
     }
 
     choosePayment(plan: Plan) {
-        var _this = this;
-        // let key = 'pk_test_FX2nzQcClgXqETUTMZDK2BNu';
-        let key = 'pk_live_Zo2921HDEtTxDEZfXS3ZVR5N';
-        StripeCheckout.configure({
-            key: key,
-            name: 'GaiGai Pte Ltd',
-            description: plan.itemName,
-            image: 'https://www.letsgaigai.com/gokaikai/assets/images/big.logo.png',
-            allowRememberMe: false,
-            panelLabel: plan.count ? 'Subscribe' : 'Pay S$' + plan.amount,
-            token: function (token: any) {
-                plan.tokenId = token.id;
-                plan.email = token.email;
-                console.log(plan);
-                _this.stripeService.pay(plan).subscribe(result => _this.router.navigate(['/pay', result]));
-            }
-        }).open();
+        this.router.navigate(['/pay', plan]);
     }
 }
